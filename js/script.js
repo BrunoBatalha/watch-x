b33ee1d2eb8a3d54acb91b75fce039b9ba4a1014
Bruno Batalha <brunobatalha37@gmail.com>
2020-08-31 15:40:15 -0400

    Adiciona animações

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// window.Anime = class {
+//   constructor() {
+//     (this.items = document.querySelectorAll("[data-anime]")), this.init();
+//   }
+//   init() {
+//     this.items.forEach(item => {
+//         setTimeout(()=>{
+//             item.classList.add("anime");
+//         },Number(item.getAttribute('data-anime')))
+//     });
+//   }
+// };
+// if (window.Anime) {
+//   new Anime();
+// }
